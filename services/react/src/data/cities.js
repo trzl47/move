3ee2d857aa0f364bec99422dc82a3a1b10bf7e1b
trzl <brownty47@gmail.com>
2018-06-26 20:11:01 +0000

    Moving cities into imported data module
    this will better simulate using external data sources and further separate concerns

---
+++ services/react/src/data/cities.js
@@ -0,0 +1,24 @@
+const cities = [
+	'Tokyo',
+	'Seoul',
+	'Mexico City',
+	'New York',
+	'Mumbai',
+	'Jakarta',
+	'Sao Paulo',
+	'Delhi',
+	'Keihanshin',
+	'Shanghai',
+	'Manila',
+	'Hong Kong',
+	'Los Angeles',
+	'Kolkata',
+	'Moscow',
+	'Cairo',
+	'Buenos Aires',
+	'London',
+	'Beijing',
+	'Karachi'
+];
+
+export default cities;
